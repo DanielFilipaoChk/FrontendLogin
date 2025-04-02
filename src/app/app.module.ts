ed4db5ea4ed0946a560b861bb9fbab09bde281a6
DanielFilipaoChk <d.checa13@gmail.com>
2025-04-02 12:25:16 -0500

    actualizacion del appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingInComponent } from './components/sing-in/sing-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SingInComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SpinnerComponent
+  ],
+  imports: [CommonModule, BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
